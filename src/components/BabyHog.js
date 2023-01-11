752e891dcae19b0aac143f2045877eb8e26ae8aa
Walker Le <walker.le88@gmail.com>
2023-01-11 10:40:30 -0700

    complete

--- src/components/BabyHog.js
+++ src/components/BabyHog.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import normalBaby from "../assets/unadulterated-hoglette.png";
+import normalBaby from "../assets/unadulterated-hoglette.png";
 import SunBaby from "../assets/sun-eyes.png";
 import BlueBaby from "../assets/blue-eyes.png";
 import GlowingBaby from "../assets/glowing-eyes.png";
@@ -8,7 +8,11 @@ import { useState } from "react";
 // is there a way we could associate eye color string values with images?
 // perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
 
-const eyeColorMapper = (color) => {
+function BabyHog({ name, hobby, eyeColor }) {
+
+  const [ hogWeight, setHogWeight ] = useState(10)
+
+  const eyeColorMapper = (color) => {
   switch(color) {
     case "blue":
       return BlueBaby;
@@ -16,31 +20,31 @@ const eyeColorMapper = (color) => {
       return SunBaby
     case "glowing":
       return GlowingBaby;
+    default: 
+      return normalBaby;
   }
 }
 
-function BabyHog({ name, hobby, eyeColor }) {
-  // function handleChangeWeight(e) {
-  //   // how can we reuse this for both buttons?
-  //   // perhaps something with e.target.name === "+"
-
-  //   const [ hogWeight, setHogWeight ] = useState(10)
-    
-  //   if(e.target.name === "+") {
-      
-  //   } else if(e.target.name === "-") {
-
-  //   }
-  // }
+  function handleChangeWeight(e) {
+    // how can we reuse this for both buttons?
+    // perhaps something with e.target.name === "+"
+    if(e.target.name === "+") {
+      setHogWeight(prevValue => prevValue + 2);
+    } else if(e.target.name === "-") {
+      if(hogWeight !== 0){
+        setHogWeight(prevValue => prevValue - 2);
+      }
+    }
+  }
   return (
     <li className="hogbabies">
       <h1>{name}</h1>
-      <h3>Weight:</h3>
+      <h3>Weight:{hogWeight}</h3>
       <h3>Hobby:{hobby}</h3>
       <h4>Eye Color:{eyeColor}</h4>
 
-      <button /*onClick={handleChangeWeight}*/ name="+">Increase Weight</button>
-      <button /*onClick={handleChangeWeight}*/ name="-">Decrease Weight</button>
+      <button onClick={handleChangeWeight} name="+">Increase Weight</button>
+      <button onClick={handleChangeWeight} name="-">Decrease Weight</button>
 
       <div className="hb-wrap">
         <img
@@ -51,13 +55,6 @@ function BabyHog({ name, hobby, eyeColor }) {
       </div>
     </li>
   );
-  
-  
-  // const babyHogGroup = offspring.map(baby => {
-    
-  // })
-  // return babyHogGroup;
-  
 }
 
 export default BabyHog;
