b6ef4151c7b2bb07eb99f410b9c32fbcf6076352
Walker Le <walker.le88@gmail.com>
2023-01-11 10:13:26 -0700

    working to finish weight

--- src/components/BabyHog.js
+++ src/components/BabyHog.js
@@ -1,37 +1,63 @@
 import React from "react";
-import normalBaby from "../assets/unadulterated-hoglette.png";
+// import normalBaby from "../assets/unadulterated-hoglette.png";
 import SunBaby from "../assets/sun-eyes.png";
 import BlueBaby from "../assets/blue-eyes.png";
 import GlowingBaby from "../assets/glowing-eyes.png";
+import { useState } from "react";
 
 // is there a way we could associate eye color string values with images?
 // perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
 
-function BabyHog(props) {
-  function handleChangeWeight(e) {
-    // how can we reuse this for both buttons?
-    // perhaps something with e.target.name === "+"
+const eyeColorMapper = (color) => {
+  switch(color) {
+    case "blue":
+      return BlueBaby;
+    case "sun":
+      return SunBaby
+    case "glowing":
+      return GlowingBaby;
   }
+}
 
+function BabyHog({ name, hobby, eyeColor }) {
+  // function handleChangeWeight(e) {
+  //   // how can we reuse this for both buttons?
+  //   // perhaps something with e.target.name === "+"
+
+  //   const [ hogWeight, setHogWeight ] = useState(10)
+    
+  //   if(e.target.name === "+") {
+      
+  //   } else if(e.target.name === "-") {
+
+  //   }
+  // }
   return (
     <li className="hogbabies">
-      <h1>Name</h1>
+      <h1>{name}</h1>
       <h3>Weight:</h3>
-      <h3>Hobby:</h3>
-      <h4>Eye Color:</h4>
+      <h3>Hobby:{hobby}</h3>
+      <h4>Eye Color:{eyeColor}</h4>
 
-      <button name="+">Increase Weight</button>
-      <button name="-">Decrease Weight</button>
+      <button /*onClick={handleChangeWeight}*/ name="+">Increase Weight</button>
+      <button /*onClick={handleChangeWeight}*/ name="-">Decrease Weight</button>
 
       <div className="hb-wrap">
         <img
-          src={normalBaby}
+          src={eyeColorMapper(eyeColor)}
           style={{ height: "200px" }}
           alt="MasterBlasterJrJr"
         />
       </div>
     </li>
   );
+  
+  
+  // const babyHogGroup = offspring.map(baby => {
+    
+  // })
+  // return babyHogGroup;
+  
 }
 
 export default BabyHog;
--- src/components/HogBoss.js
+++ src/components/HogBoss.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Boss from "../assets/boss-hog.png";
 import BabyHog from "./BabyHog";
-// import offspring from "../data.js"
+import offspring from "../data.js"
 
 function HogBoss() {
   const [eyeColor, setEyeColor] = useState("blue");
@@ -9,6 +9,12 @@ function HogBoss() {
   function handleChangeEyeColor(e) {
     setEyeColor(e.target.value);
   }
+
+  const babyGroup = offspring.map(baby => {
+    return (
+      <BabyHog key={baby.id} name={baby.name} hobby={baby.hobby} eyeColor={eyeColor}/>
+    )
+  })
 
   return (
     <div>
@@ -40,9 +46,7 @@ function HogBoss() {
         <img id="boss-blaster" src={Boss} alt="" />
       </div>
       <ul className="hoglist">
-        <BabyHog />
-        <BabyHog />
-        <BabyHog />
+        {babyGroup}
       </ul>
     </div>
   );
